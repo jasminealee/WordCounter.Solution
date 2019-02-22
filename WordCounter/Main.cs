cd0a49c003c835a0dce09a4e95f54051a03b4ed7
Jasmine Lee <jasmine.al1722@gmail.com>
2019-02-22 11:51:44 -0800

    last commit was in the wrong directory; add img directory splash.jpg image and more to main.cs

--- WordCounter/Main.cs
+++ WordCounter/Main.cs
@@ -4,3 +4,15 @@ using System.Collections.Generic;
 namespace WordCounter {
 
   public class Program
+  {
+    public static void Main(string[] args)
+    {
+      WordFindClass userFind = new WordFindClass();
+      Console.WriteLine("Enter your sentence:");
+      string inputtedSentence = Console.ReadLine();
+      string inputtedSentence = inputtedSentence.ToLower();
+      Console.WriteLine("Select your word:");
+      string selectedWord = Console.ReadLine();
+      string selectedWord = selectedWord.ToLower();
+      userFind.StringSplitWord(selectedSentence, selectedWord);
+      
