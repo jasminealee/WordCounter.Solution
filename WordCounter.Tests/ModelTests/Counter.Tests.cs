7ab2eb97cc94ba2d381eb67c6f1d492db913763e
Jasmine Lee <jasmine.al1722@gmail.com>
2019-02-22 09:27:32 -0800

    add intro to Counter.Test.cs

---
+++ WordCounter.Tests/ModelTests/Counter.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Main.Models;
+
+namespace Main.Tests;
+
+{
+  [TestClass]
+  public class CounterWord.TestTests
+  {
+    [TestMethod]
+    public void 
+  }
+}
