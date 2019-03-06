e921fe90976a714cdd405969d1a2fc1459087198
Jasmine Lee <jasmine.al1722@gmail.com>
2019-03-05 22:45:39 -0800

    add all info in WordCounterControllerTests.cs

--- WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -1,1 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Controllers;
+using WordCounter.Models;
 
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterControllerTest
+  {
+    [TestMethod]
+    public void Index_ReturnAccurateView_True()
+    {
+			WordCounterController controller = new WordCounterController();
+      ActionResult indexView = controller.Index();
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+		}
+
+    [TestMethod]
+    public void New_ReturnAccurateView_True()
+    {
+			WordCounterController controller = new WordCounterController();
+      ActionResult indexNew = controller.New();
+      Assert.IsInstanceOfType(indexNew, typeof(ViewResult));
+		}
+
+    [TestMethod]
+    public void Action_ReturnAccurateAction_RedirectToActionResult()
+    {
+		  WordCounterController controller = new WordCounterController();
+      IActionResult view = controller.Action("I have three types of bananas", "bananas");
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    }
+
+    [TestMethod]
+    public void Action_RedirectToAccurateAction_Index()
+    {
+      WordCounterController controller = new WordCounterController();
+      RedirectToActionResult actionResult = controller.Action("I have three types of bananas", "bananas") as RedirectToActionResult;
+      string counter = actionResult.ActionName;
+      Assert.AreEqual(counter, "Index");
+    }
+  }
+}
